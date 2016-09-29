998bc534ad031b6949fa4be25388e397a8087154
jifengdehao <2975830779@qq.com>
2016-09-30 00:49:53 +0800

    blog 第5节

--- myapp/config/routes.js
+++ myapp/config/routes.js
@@ -1,5 +1,9 @@
 var ModelUser=require('../model/user');
-//console.log(ModelUser);
+var ModelBlog=require('../model/blog');
+
+
+
+
 module.exports=function(app){
 	
 	//中间件
@@ -13,8 +17,6 @@ module.exports=function(app){
 		console.log(user);
 		next();
 	});
-	
-	
 	
 	app.get('/',function(req,res,next){
 		//res.send('这是我导出去！！');
@@ -86,7 +88,76 @@ module.exports=function(app){
 	
 	
 	app.get('/user/:_id',function(req,res,next){
-		res.send('用户中心正在开发')
+		
+		var getData={
+			_id:req.param("_id")
+		}
+		ModelUser.findById(getData,function(error,data){
+			if(error) console.log(error);		
+			//console.log(data);
+			if(data){
+				res.render('user',{
+					title:data.name +'的个人资料',
+					view:data
+				});
+			}else{
+				res.send('查询不到你的资料');
+			}
+		});
+	
+		//res.send('用户中心正在开发')
 	});
+	
+	//博客添加内容和标题
+	app.get('/add',function(req,res,next){
+		res.render('add',{
+			title:"发表"
+		});
+	});
+	app.post('/add',function(req,res,next){
+		var postData={
+			title:req.body.title,
+			content:req.body.content
+		}
+		console.log(postData);
+		
+		ModelBlog.create(postData,function(err,data){
+			if(err) console.log(err);
+			//res.send(data);
+			res.redirect('/view/'+data._id);
+		});
+		//res.send('发表成功');
+	});
+	
+	app.get('/view',function(req,res,next){
+		ModelBlog.find({},function(err,data){
+		   //若没有向find传递参数，默认的是显示所有文档
+		   if(err) console.log(err);
+		   if(data){
+		   	    res.send(data);
+		   }
+		});
+	});
+	app.get('/view/:_id',function(req,res,next){
+		var getData={
+			_id:req.param("_id")
+		}
+		
+		ModelBlog.findById(getData,function(err,data){
+			if(err) cosnole.log(err);
+			if(data){
+				res.render('view',{
+					title:data.title,
+					content:data.content
+				});
+			}else{
+				res.send('不存在');
+			}
+		});
+		
+		
+		
+	});
+	
 	
 }
